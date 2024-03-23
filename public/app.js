fbc32c19e8769f9a750310c6ee6427fa168172c4
suttonj07 <suttonj07@gmail.com>
2024-03-23 13:51:44 -0400

    part 3

--- public/app.js
+++ public/app.js
@@ -4,11 +4,105 @@
 
 // Create map
 
+let map
+
+function buildMap(coords) {
+    map = L.map('map').setView([coords[0], coords[1]], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([coords[0], coords[1]]).addTo(map)
+        .bindPopup('You are here')
+        .openPopup();
+}
+
+// get coordinates via geolocation api
+async function getCoords() {
+    const pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    });
+    return [pos.coords.latitude, pos.coords.longitude]
+}
 
 
+
+function addMarkers() {
+		for (var i = 0; i < this.businesses.length; i++) {
+		this.markers = L.marker([
+			this.businesses[i].lat,
+			this.businesses[i].long,
+		])
+			.bindPopup(`<p1>${this.businesses[i].name}</p1>`)
+			.addTo(this.map)
+		}
+	}
+
+
+async function getCoordinates() {
+    const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+    return [position.coords.latitude, position.coords.longitude]
+}
+
+async function getFoursquare(business) {
+	const options = {
+		method: 'GET',
+		headers: {
+		Accept: 'application/json',
+		Authorization: 'fsq3ATzZbmcGhdeFafr73wZcnJ+LlN6bK+4dh19a7ClS4u8='
+		}
+	}
+    let limit = 5
+	let lat = myMap.coordinates[0]
+	let lon = myMap.coordinates[1]
+	let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let data = await response.text()
+	let parsedData = JSON.parse(data)
+	let businesses = parsedData.results
+	return businesses
+}
+
+document.getElementById('submit').addEventListener('click', async (event) => {
+	event.preventDefault()
+	let business = document.getElementById('business').value
+	let data = await getFoursquare(business)
+	myMap.businesses = processBusinesses(data)
+	myMap.addMarkers()
+})
+
+function processBusinesses(data) {
+	let businesses = data.map((element) => {
+		let location = {
+			name: element.name,
+			lat: element.geocodes.main.latitude,
+			long: element.geocodes.main.longitude
+		};
+		return location
+	})
+	return businesses
+}
+
+
+
+document.getElementById('submit').addEventListener('click', async (event) => {
+	event.preventDefault()
+	let business = document.getElementById('business').value
+	let data = await getFoursquare(business)
+	myMap.businesses = processBusinesses(data)
+	myMap.addMarkers()
+})
+
+
+window.onload = async () => {
+    const coordinates = await getCoords()
+    buildMap(coordinates)
+}
 
 // Add openstreetmap tiles
-
+// add openstreetmap tiles
 
 
 //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
@@ -16,17 +110,39 @@
 
 // Create and add a geolocation marker
 
+// const marker = L.marker([48.87007, 2.346453])
+// marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
 
-// Draw the 2nd arrondissement
+// Draw the 2nd arrondissement/neighborhood
+// const latlngs = [[48.863368120198004, 2.3509079846928516],[48.86933262048345, 2.3542531602919805],[48.87199261164275, 2.3400569901592183],[48.86993336274516, 2.3280142476578813], [48.86834104280146, 2.330308418109664]]
 
+// const polygon = L.polygon(latlngs, {
+//     color: 'blue', 
+//     fillOpacity: 0.0
+// }).addTo(myMap)
 
-
+// Create red pin marker
+// const redPin = L.icon({
+//     iconUrl: './assets/red-pin.png',
+//     iconSize:     [38, 38], // size of the icon
+//     iconAnchor:   [19, 38], // point of the icon which will correspond to marker's location
+//     popupAnchor:  [0, -38] // point from which the popup should open relative to the iconAnchor
+// });
 
 // Metro station markers
 
+// const rS = L.marker([48.866200610611926, 2.352236247419453],{icon: redPin}).bindPopup('Réaumur-Sébastopol')
+// const sSD = L.marker([48.869531786321566, 2.3528590208055196],{icon: redPin}).bindPopup('Strasbourg-Saint-Denis')
+// const sentier = L.marker([48.8673721067762, 2.347107922912739],{icon: redPin}).bindPopup('Sentier')
+// const bourse = L.marker([48.86868503971672, 2.3412285142058167],{icon: redPin}).bindPopup('Bourse')
+// const qS = L.marker([48.869560129483226, 2.3358638645569543],{icon: redPin}).bindPopup('Quatre Septembre')
+// const gB = L.marker([48.871282159004856, 2.3434818588892714],{icon: redPin}).bindPopup('Grands Boulevards')
+
+// const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap);
 
 
 
-// Create red pin marker
+
+
